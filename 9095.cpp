5deb36970956a3663a7d9cadbf2638cc38486e6a
rpopic2 <rpopic2@gmail.com>
2022-11-07 23:44:29 +0900

    solve 9095(only dp part)

--- 9095.cpp
+++ 9095.cpp
@@ -8,22 +8,27 @@ int a[MAX];
 
 int main()
 {
+    //int T;
+    //cin >> T;
+    //int ns[T];
+    //for (int i = 0; i < T; i++)
+    //{
+        //cin >> ns[i];
+    //}
     a[0] = 1;
     int n;
-    int result = 0;
     cin >> n;
 
     for (int k = 1; k <= n; k++)
     {
         for (int i = 1; i <= 3; i++)
         {
-            int j = n - i;
+            int j = k - i;
             a[k] += a[j];
-            result += a[j];
         }
     }
 
-    cout << result;
+    cout << a[n];
     cout << endl;
     return 0;
 }
