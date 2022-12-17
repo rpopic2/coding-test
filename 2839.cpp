46af1db951683fc2d86f052764244873a033df51
rpopic2 <rpopic2@gmail.com>
2022-12-18 01:29:15 +0900

    edit 2839

--- 2839.cpp
+++ 2839.cpp
@@ -1,30 +1,27 @@
 #include <iostream>
 #include <algorithm>
 #include <array>
-constexpr int MAX_INPUT = 5000;
+using namespace std;
+constexpr int MAX_NUM = 5000;
 
 int main()
 {
-    std::array<int, MAX_INPUT> results{ 0 };
-    int n_input;
-    std::cin >> n_input;
-
+    array<int, MAX_NUM> results{ 0 };
     results[3] = 1;
     results[5] = 1;
+    int n_input;
+    cin >> n_input;
 
     for (int i = 5 + 1; i <= n_input; ++i)
     {
-        int m_three = results[i - 3];
-        int m_five = results[i - 5];
-        int tmp;
-        int min = std::min(m_three , m_five);
-        if (m_three == m_five && m_three == 0) continue;
-        if (min == 0) tmp = std::max(m_five, m_three);
-        else tmp = min;
-            results[i] = ++tmp;
+        array<int, 2> a { results[i - 3], results[i - 5] };
+        if (a[0] > a[1]) swap(a[0], a[1]);
+        int &j = results[i];
+        j = a[0] == 0 ? a[1] : a[0];
+        if (j != 0) ++j;
     }
     int answer = results[n_input];
     if (answer == 0) answer = -1;
 
-    std::cout << answer;
+    cout << answer;
 }
