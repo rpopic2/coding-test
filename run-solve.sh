17e572aed2137d560faa305b8974380b326108ba
rpopic2 <rpopic2@gmail.com>
2022-12-21 01:58:04 +0900

    improve run-solve

--- run-solve.sh
+++ run-solve.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-clang++ -Wall -std=c++17 $1;
-time ./a.out
+FT=$(echo $1 | cut -d. -f2)
+if test "$FT" ==  "cpp"; then
+    clang++ -Wall -std=c++17 $1;
+elif test "$FT" == "c"; then
+    clang $1;
+fi
+
+if test $? -eq 0; then
+    time ./a.out
+fi
