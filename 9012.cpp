e7a3b34fb372533a362f6efb4fd5e54a4c275524
rpopic2 <rpopic2@gmail.com>
2022-12-21 00:56:38 +0900

    solve 9012

--- 9012.cpp
+++ 9012.cpp
@@ -1,28 +1,25 @@
 #include <iostream>
-constexpr char POPEN = '(', PCLOSE = ')';
+constexpr int BIG_NUM = 1 << 16;
+
+void cnt_parens(std::string &buf)
+{
+    if (std::cin.peek() == EOF) return;
+    int paren_to_resolve = 0;
+    char c;
+    while (std::cin.get(c))
+    {
+        if (c == '(') ++paren_to_resolve;
+        else if (c == ')') --paren_to_resolve;
+        if (c == '\n' || paren_to_resolve < 0) break;
+    }
+    buf += paren_to_resolve == 0 ? "YES\n" : "NO\n";
+    if (c != '\n') std::cin.ignore(BIG_NUM, '\n');
+}
 
 int main()
 {
-    int paren_to_resolve = 0;
-    char c;
-    std::string buf;
-
+    std::string buf = "";
     std::cin.ignore(1 << 16, '\n');
-    while (std::cin.get(c))
-    {
-        if (c == POPEN) ++paren_to_resolve;
-        else if (c == PCLOSE) --paren_to_resolve;
-        if (c == '\n')
-        {
-            buf += paren_to_resolve == 0 ? "YES\n" : "NO\n";
-            paren_to_resolve = 0;
-        }
-        if (paren_to_resolve < 0)
-        {
-            std::cin.ignore(1 << 16, '\n');
-            buf += "NO\n";
-            paren_to_resolve = 0;
-        }
-    }
+    while (!std::cin.eof()) cnt_parens(buf);
     std::cout << buf;
 }
