f9c6289acbd6e4edd860469960b6cd9f93995fb3
rpopic2 <rpopic2@gmail.com>
2023-01-26 04:48:44 +0900

    solve 10953, 2750

--- fib-top-down.cpp
+++ fib-top-down.cpp
@@ -4,11 +4,10 @@
 std::vector<int> fibonacci_arr;
 int fibonacci(int n) {
     if (n < 2) return n;
-    if (fibonacci_arr[n] != 0) {
-        return fibonacci_arr[n];
-    }
-    fibonacci_arr[n] = fibonacci(n - 2) + fibonacci(n - 1);
-    return fibonacci_arr[n];
+    int &fib_n = fibonacci_arr[n];
+    if (fib_n != 0) return fib_n;
+    fib_n = fibonacci(n - 2) + fibonacci(n - 1);
+    return fib_n;
 }
 
 int main() {
