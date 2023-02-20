6e730c3a20dfb7f165124d0b834e190588b6c2b5
rpopic2 <rpopic2@gmail.com>
2023-02-20 10:17:04 +0900

    testing iter-test

--- iter-test.cpp
+++ iter-test.cpp
@@ -1,39 +1,59 @@
 #include <iostream>
 #include <chrono>
 #include <cmath>
-#include<vector>
-using namespace std;
-using namespace chrono;
+#include <vector>
 
-vector<int> v1(10000000, 0);
-vector<int> v2(10000000, 0);
+constexpr int N = 10'000'000;
 
 void iterFor()
 {
-    auto e = v2.end();
+	std::vector<int> v2(N, 0);
 
-    system_clock::time_point start = system_clock::now();
-
-    for (auto iter = v2.begin(); iter != e; ++iter)
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    auto vector_end = v2.end();
+    for (auto iter = v2.begin(); iter != vector_end; iter++)
     {
         *iter = 100;
     }
 
-    duration<double>sec = system_clock::now() - start;
-
+    std::chrono::duration<double> sec = std::chrono::high_resolution_clock::now() - start;
     std::cout << "Iterfor : " << sec.count() << '\n';
+}
+void expanded_for() {
+	std::vector<int> vec(N, 0);
+
+     auto && range = vec;
+     for (auto begin = std::begin(range), end = std::end(range); begin != end; ++begin)
+     {
+        auto & rg = *begin;
+        rg = 100;
+     }
+
+    auto start = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> sec = std::chrono::high_resolution_clock::now() - start;
+    std::cout << "expanded_for : " << sec.count() << '\n';
+
 }
 void RBfor()
 { 
-    system_clock::time_point start = system_clock::now();
+	std::vector<int> vec(N, 0);
 
+    auto start = std::chrono::high_resolution_clock::now();
 
-    for (auto& rg : v1)
+    for (auto& rg : vec)
     {
         rg = 100;
     }
+    // is equivalant to
+    // auto && range = vec;
+    // for (auto begin = std::begin(range), end = std::end(range); begin != end; ++begin)
+    // {
+    //    auto & rg = *begin;
+    //    rg = 100;
+    // }
 
-    duration<double>sec = system_clock::now() - start;
+    std::chrono::duration<double> sec = std::chrono::high_resolution_clock::now() - start;
     std::cout << "RBfor : " << sec.count() << '\n';
 }
 
@@ -41,5 +61,6 @@ void RBfor()
 int main()
 {
     iterFor();
+    expanded_for();
     RBfor();
 }
