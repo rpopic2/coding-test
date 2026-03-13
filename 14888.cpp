660aca9713a3c262e79af218f74f08f626fe9388
gpopic <8055265+rpopic2@users.noreply.github.com>
2026-03-13 23:49:39 +0900

    solve

--- 14888.cpp
+++ 14888.cpp
@@ -1,16 +1,57 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+
+int n;
+std::vector<int> as;
+int maxx = INT_MIN;
+int minx = INT_MAX;
+
+union {
+    struct {
+        int add, sub, mul, div;
+    };
+    int arr[4];
+} u;
+
+void recurse(int i, int acc) {
+    if (i >= n) {
+        maxx = std::max(maxx, acc);
+        minx = std::min(minx, acc);
+        return;
+    }
+    int a = as[i];
+    for (int j = 0; j < 4; ++j) {
+        if (u.arr[j] == 0) {
+            continue;
+        }
+
+        u.arr[j] -= 1;
+        int result = acc;
+        switch (j) {
+        case 0: result += a; break;
+        case 1: result -= a; break;
+        case 2: result *= a; break;
+        case 3: result /= a; break;
+        }
+
+        recurse(i + 1, result);
+        u.arr[j] += 1;
+    }
+}
 
 int main() {
-    int n;
     std::cin >> n;
+    as = std::vector<int>(n);
 
-    std::vector<int> as(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> as[i];
     }
 
-    int add, sub, mul, div;
-    std::cin >> add >> sub >> mul >> div;
+    std::cin >> u.add >> u.sub >> u.mul >> u.div;
+
+    recurse(1, as[0]);
+
+    std::cout << maxx << '\n' << minx << '\n';
 }
 
